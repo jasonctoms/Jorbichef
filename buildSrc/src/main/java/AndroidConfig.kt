49a4552530b6f3a59ff4e3f19c12a25a16ebe363
Jason <jasonctoms@gmail.com>
2021-11-26 18:07:46 +0100

    Make project build again

--- buildSrc/src/main/java/AndroidConfig.kt
+++ buildSrc/src/main/java/AndroidConfig.kt
@@ -2,9 +2,9 @@ import org.gradle.api.JavaVersion
 
 object AndroidConfig {
     const val min = 26
-    const val compile = 30
+    const val compile = 31
     const val target = compile
-    const val buildToolsVersion = "30.0.3"
+    const val buildToolsVersion = "31.0.0"
     const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
     const val applicationId = "com.jorbital.jorbichef"
