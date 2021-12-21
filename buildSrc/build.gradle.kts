be89fd92100af3dc42de238c187623ad55c684e8
Jason Toms <jasonctoms@gmail.com>
2021-12-21 09:18:30 +0100

    Gradle update

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,7 +10,7 @@ repositories {
 
 dependencies {
     compileOnly(gradleApi())
-    implementation("com.android.tools.build:gradle:7.1.0-beta04")
+    implementation("com.android.tools.build:gradle:7.1.0-beta05")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
     implementation("com.google.gms:google-services:4.3.10")
     implementation("org.jlleitschuh.gradle:ktlint-gradle:10.2.0")
